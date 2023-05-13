2dbeaa1dd90bd01561ff50b235df3be3d8e9a58e
Joyce Chen <joyce.chen721@gmail.com>
2023-05-13 15:45:29 -0700

    set up backend
    updated gitignore (don't include any path leading to node modules and next). set up express port (4000) and controllers page. routes will be pre-appended with /bruinshare

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import serverRoutes from './routes/requests.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json());
+// app.use(cors());
+
+app.use('/bruinshare', serverRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server is listening on port:${PORT}`));
