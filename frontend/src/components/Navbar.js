c4718b02c486fe100315b1ea7e98d0ba5225ab37
roland yang <rolandyang@gmail.com>
2023-05-18 14:46:01 -0700

    Navbar Layout
    Layout of navbar finished just needs styling theme
    
    Typography doesn't render though bruh wtf is wrong with this

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from 'next/router';
-import Button from '@mui/material/Button';
+import { AppBar, Toolbar, Stack, Typography } from '@mui/material';
 
 function Navbar (props, {user}) {
   const router = useRouter();
@@ -12,26 +12,41 @@ function Navbar (props, {user}) {
   }
 
   return (
-    <div>
-      <Button variant="contained">MaterialUI Test Button</Button>
-        {props.profile && 
-          <div>
-            <Link href="/">HOME</Link>
-            <br></br>
-            <Link href="/profile">USER PROFILE</Link>
-            <br></br>
-            <Link href="/login" onClick={logout}>LOG OUT</Link>
-          </div>
-          }
-          {!props.profile && 
-          <div>
-            <Link href="/login">LOGIN</Link>
-            <br></br>
-            <Link href="/signup">SIGN UP</Link>
-            <br></br>
-          </div>
-          }
-    </div>
+    <>
+      <AppBar position='static' color="transparent" style={{ backgroundColor: 'transparent' }}>
+          <Toolbar >
+              <Link href="/"><img src="icons/logo.svg" alt="Logo" height="30" /></Link>
+              <Typography sx={{flexGrow:1}}></Typography>
+              <Stack direction='row' spacing={5}>
+                  <Link href="/"><Typography variant='h6'>Home</Typography></Link>
+                  <Link href="/"><Typography variant='h6'>MyRides</Typography></Link>
+                  <Link href="/"><Typography variant='h6'>Profile</Typography></Link>
+                  <Link href="/"><Typography variant='h6'>Logout</Typography></Link>
+              </Stack>
+          </Toolbar>
+      </AppBar>
+    </>
+    // <div>
+    //   <Button variant="contained">MaterialUI Test Button</Button>
+    //   <Typography>Test</Typography>
+    //     {props.profile && 
+    //       <div>
+    //         <Link href="/">HOME</Link>
+    //         <br></br>
+    //         <Link href="/profile">USER PROFILE</Link>
+    //         <br></br>
+    //         <Link href="/login" onClick={logout}>LOG OUT</Link>
+    //       </div>
+    //       }
+    //       {!props.profile && 
+    //       <div>
+    //         <Link href="/login">LOGIN</Link>
+    //         <br></br>
+    //         <Link href="/signup">SIGN UP</Link>
+    //         <br></br>
+    //       </div>
+    //       }
+    // </div>
   );
 };
 
