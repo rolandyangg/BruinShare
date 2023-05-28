0c536502a1a14f0b1aba6d536fdcbdf7ac8cc1f8
flobythebay <florence.y.zhao@gmail.com>
2023-05-28 01:53:43 -0700

    add styling for create post dialog

--- frontend/src/components/formDialog.js
+++ frontend/src/components/formDialog.js
@@ -1,16 +1,9 @@
 import * as React from 'react';
-//import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
-//import { styled } from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
-//import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-//import IconButton from '@mui/material/IconButton';
-//import CloseIcon from '@mui/icons-material/Close';
-//import Typography from '@mui/material/Typography';
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import styles from "@/styles/postDialog.module.css";
+import styles from '@/styles/postDialog.module.css';
 
 export default function CustomizedDialogs() {
   const [open, setOpen] = React.useState(false);
@@ -18,6 +11,7 @@ export default function CustomizedDialogs() {
   const handleClickOpen = () => {
     setOpen(true);
   };
+
   const handleClose = () => {
     setOpen(false);
   };
@@ -30,55 +24,64 @@ export default function CustomizedDialogs() {
 
   return (
     <div>
-        <Button variant="outlined" onClick={handleClickOpen}>
-            Create Post
-        </Button>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogContent >
-            <h3>Create Post</h3>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Create Post
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <div className={styles.dialogContainer}>
+          <h3 className={styles.dialogTitle}>Create Post</h3>
+          <DialogContent className={styles.dialogContent}>
             <form onSubmit={(e) => addChild(e)}>
-              Depart. Location:
-              <input type="text" name="departLoc" required />
-              <br />
-              Destination:
-              <input type="text" name="dest" required />
-              <br />
-              Depart. Date:
-              <input type="text" name="departDate" required />
-              <br />
-              Depart. Time:
-              <input type="text" name="departTime" required />
-              <br />
-              Flight Time:
-              <input type="text" name="flightTime" required />
-              <br />
-              Flight Number:
-              <input type="text" name="flightNumber" required />
-              <br />
-              Flight Dest.:
-              <input type="text" name="flightDest" required />
-              <br />
-              <label htmlFor="number">Group Size (including you):</label>
-              <FormControl>
-                <Select value={selectedNumber} onChange={handleNumberChange}>
-                <MenuItem value={2}>2</MenuItem>
-                <MenuItem value={3}>3</MenuItem>
-                <MenuItem value={4}>4</MenuItem>
-                <MenuItem value={5}>5</MenuItem>
-                <MenuItem value={6}>6</MenuItem>
-                <MenuItem value={7}>7</MenuItem>
-                <MenuItem value={8}>8</MenuItem>
-                {/* Add more MenuItems with desired numbers */}
-                </Select>
-              </FormControl>
-              <br />
-              <DialogActions>
+              <div className={styles.inputContainer}>
+                <label htmlFor="departLoc">Depart. Location:</label>
+                <input type="text" id="departLoc" name="departLoc" required />
+              </div>
+              <div className={styles.inputContainer}>
+                <label htmlFor="dest">Destination:</label>
+                <input type="text" id="dest" name="dest" required />
+              </div>
+              <div className={styles.inputContainer}>
+                <label htmlFor="departDate">Depart. Date:</label>
+                <input type="text" id="departDate" name="departDate" required />
+              </div>
+              <div className={styles.inputContainer}>
+                <label htmlFor="departTime">Depart. Time:</label>
+                <input type="text" id="departTime" name="departTime" required />
+              </div>
+              <div className={styles.inputContainer}>
+                <label htmlFor="flightTime">Flight Time:</label>
+                <input type="text" id="flightTime" name="flightTime" required />
+              </div>
+              <div className={styles.inputContainer}>
+                <label htmlFor="flightNumber">Flight Number:</label>
+                <input type="text" id="flightNumber" name="flightNumber" required />
+              </div>
+              <div className={styles.inputContainer}>
+                <label htmlFor="flightDest">Flight Dest.:</label>
+                <input type="text" id="flightDest" name="flightDest" required />
+              </div>
+              <div className={styles.inputContainer}>
+                <label htmlFor="number">Group Size (including you):</label>
+                <FormControl>
+                  <Select value={selectedNumber} onChange={handleNumberChange}>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={6}>6</MenuItem>
+                    <MenuItem value={7}>7</MenuItem>
+                    <MenuItem value={8}>8</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              <div className={styles.buttonContainer}>
                 <Button onClick={handleClose}>Cancel</Button>
                 <Button type="submit">Publish</Button>
-              </DialogActions>
+              </div>
             </form>
           </DialogContent>
-        </Dialog>
-      </div>
+        </div>
+      </Dialog>
+    </div>
   );
 }
