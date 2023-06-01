d7af532c9921acb50e84d16a5f1f996ed54f3a3d
Peony Mong <pmonfcusd@gmail.com>
2023-06-01 00:07:51 -0700

    Corrected some of the crude code from prev commit.

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,37 +1,42 @@
 import { useEffect } from "react";
 import styles from "@/styles/Profile.module.css";
+import Image from "next/image";
+import Navbar from '@/Navbar.js';
 
 export default function Profile({profile}) {
     return (
-      <div class="profile-container">
-        <div class="profile-left">
-          <div class="profile-image">
-            <img src="profile_picture.jpg" alt="Profile Picture" class="center" />
+     <div>
+      <Navbar/>
+      <div class={styles.profile-container}>
+        <div class={styles.profile-left}>
+          <div class={styles.profile-image}>
+          <img src="" alt="Profile Picture" class={styles.center} />
           </div>
-          <h1 class="profile-name">John Doe</h1>
-          <p class="profile-username">@johndoe</p>
-          <p class="profile-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h1 class={styles.profile-name}>{profile.firstname}</h1>
+          <p class={styles.profile-username}>{profile.username}</p>
+          <p class={styles.profile-description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
-        <div class="profile-right">
-          <div class="profile-info">
-            <div class="info-row">
-              <span class="info-label">Email</span>
-              <span class="info-value">johndoe@example.com</span>
+        <div class={styles.profile-right}>
+          <div class={styles.profile-info}>
+            <div class={styles.info-row}>
+              <span class={styles.info-label}>Email</span>
+              <span class={styles.info-value}>{profile.username}</span>
             </div>
-            <div class="info-row">
-              <span class="info-label">Phone</span>
-              <span class="info-value">123-456-7890</span>
+            <div class={styles.info-row}>
+              <span class={styles.info-label}>Phone</span>
+              <span class={styles.info-value}>{profile.phone}</span>
             </div>
-            <div class="info-row">
-              <span class="info-label">Living Location</span>
-              <span class="info-value">City, Country</span>
+            <div class={styles.info-row}>
+              <span class={styles.info-label}>Living Location</span>
+              <span class={styles.info-value}>Delta Terrace</span>
             </div>
-            <div class="info-row">
-              <span class="info-label">Interests &amp; Hobbies</span>
-              <span class="info-value">Reading, Traveling, Cooking</span>
+            <div class={styles.info-row}>
+              <span class={styles.info-label}>Interests &amp; Hobbies</span>
+              <span class={styles.info-value}>This user has not provided their interests and hobbies.</span>
             </div>
           </div>
         </div>
-      </div>      
+      </div>    
+    </div>  
   );
   }
